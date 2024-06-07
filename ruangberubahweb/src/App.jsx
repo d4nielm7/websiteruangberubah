c6bafb0c2a66b495f0ccbd9f44a8eb7ddba6ecb9
DANIEL <d4nielm7@gmail.com>
2024-06-07 20:47:25 +0200

    header progress

--- ruangberubahweb/src/App.jsx
+++ ruangberubahweb/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Header from './Components/Header/header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './tailwind.css';
+
 const App = () => {
   return (
     <div classname= 'HEAD'>
--- ruangberubahweb/src/Components/Header/header.jsx
+++ ruangberubahweb/src/Components/Header/header.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
-import './header.css'
-import small_logo from '../../assets/smalllogo.png'
+import React, { useState } from 'react';
+import './header.css';
+import small_logo from '../../assets/smalllogo.png';
 
+const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-const header = () => {
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <div className='head'>
-      <img src= {small_logo} alt="" className='logo'/>
+      <div className='nav-left'>
+        <button className='hamburger' onClick={toggleNav}>
+          <i className='material-icons'>☰</i>
+        </button>
+        <img src={small_logo} alt="Logo" className='logo' />
+        {isNavOpen && (
 
-      <ul>
-        <li><a href="#About">About us</a></li>
-        <li><a href="service">Service</a></li>
-        <li><a href="#myproject">Contact</a></li>
-      </ul> 
+          <div className='side-nav'>
+            <button className='close-btn' onClick={toggleNav}>
+              <i className='material-icons'>X</i>
+            </button>
 
-      <button className='signup-button'>Sign Up</button>
+            <ul>
+              <li><a href="#About">About us</a></li>
+              <li><a href="service">Service</a></li>
+              <li><a href="#myproject">Contact</a></li>
+            </ul>
+
+          </div>
+
+        )}
+      </div>
+
+      <div className='search-container'>
+        <input type="text" className='search-bar' placeholder='Search...' />
+        <button className='signup-button'>Sign Up</button>
+      </div>
+
     </div>
-  )
-}
+  );
+};
 
-export default header
+export default Header;
