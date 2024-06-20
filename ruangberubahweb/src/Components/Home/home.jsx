d45fbbbe73f404c98c0be3ca60bb8ed23c3c89a2
DANIEL <d4nielm7@gmail.com>
2024-06-20 22:54:29 +0200

    adjustment home

--- ruangberubahweb/src/Components/Home/home.jsx
+++ ruangberubahweb/src/Components/Home/home.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import bwPodcast from '../../assets/pic/group2blackwhite.png';
 import './home.css'; 
+
 const Home = () => {
   return (
     <div 
-      className="Home bg-cover bg-center h-screen flex items-center justify-center" 
+      className="Home bg-cover bg-center h-screen flex flex-col items-center justify-center" 
       style={{ backgroundImage: `url(${bwPodcast})` }}
     >
-      <div className="text-center text-white mb-48">
-        <h1 className="text-8xl font-bold font-raleway">WELCOME ABROAD</h1>
+      <div className="text-center text-white mb-48 z-10">
+        <h1 className="text-6xl md:text-8xl font-bold font-raleway">WELCOME ABROAD</h1>
         <div className="flex justify-center items-center">
-          <p className="text-4xl font-bold font-raleway" style={{ color: '#7ed957' }}>
+          <p className="text-3xl md:text-4xl font-bold font-raleway" style={{ color: '#7ed957' }}>
             UBAH
           </p>
-          <p className="text-4xl font-bold font-raleway" style={{ color: 'white' }}>
+          <p className="text-3xl md:text-4xl font-bold font-raleway" style={{ color: 'white' }}>
             citizens!
           </p>
         </div>
