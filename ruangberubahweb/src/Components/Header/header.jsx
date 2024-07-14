d9597a72d5c53621e796c85d985b702903e4677d
DANIEL <d4nielm7@gmail.com>
2024-07-14 14:03:35 +0200

    header animation

--- ruangberubahweb/src/Components/Header/header.jsx
+++ ruangberubahweb/src/Components/Header/header.jsx
@@ -14,6 +14,7 @@ const Header = () => {
 
   const menuAnimation = useSpring({
     transform: isNavOpen ? 'translateX(0%)' : 'translateX(-100%)',
+    config: { duration: 200 },
     opacity: isNavOpen ? 1 : 0,
   });
 
@@ -31,14 +32,24 @@ const Header = () => {
     <div className='head font-league-spartan'>
       <div className='nav-left'>
         <button className='hamburger' onClick={toggleNav}>
-          <i className='material-icons'>☰</i>
+          <div className="group flex h-8 w-8 cursor-pointer items-center justify-center rounded-2xl bg-black p-1.5">
+            <div className="space-y-1">
+                <span className="block h-0.5 w-6 origin-center rounded-full bg-white transition-transform ease-in-out group-hover:translate-y-1 group-hover:rotate-45"></span>
+                <span className="block h-0.5 w-5 origin-center rounded-full bg-white transition-transform ease-in-out group-hover:w-6 group-hover:-translate-y-1 group-hover:-rotate-45"></span>
+            </div>
+          </div>
         </button>
         <img src={small_logo} alt="Logo" className='logo' onClick={handleLogoClick}/>
         <animated.div style={menuAnimation} className="side-nav">
           {isNavOpen && (
             <div className='side-nav'>
               <button className='close-btn' onClick={toggleNav}>
-                <i className='material-icons'>☰</i>
+                  <div className="group flex h-8 w-8 cursor-pointer items-center justify-center rounded-2xl bg-black p-1.5">
+                    <div className="space-y-1">
+                      <span className="block h-0.5 w-6 origin-center rounded-full bg-white transition-transform ease-in-out group-hover:translate-y-1 group-hover:rotate-45"></span>
+                      <span className="block h-0.5 w-5 origin-center rounded-full bg-white transition-transform ease-in-out group-hover:w-6 group-hover:-translate-y-1 group-hover:-rotate-45"></span>
+                    </div>
+                  </div>
               </button>
               <ul>
                 <li><a href="#About-us">About us</a></li>
@@ -53,7 +64,10 @@ const Header = () => {
           )}
         </animated.div>
       </div>
-      <button className='right-button rounded-[25px] font-raleway' onClick={handleSignInClick}>Sign In</button>
+      <button className="right-button rounded-[25px] font-raleway overflow-hidden relative group" onClick={handleSignInClick}>
+        <span className="absolute inset-0 bg-[#7ed957] transform scale-y-0 origin-bottom transition-transform duration-300 ease-in-out group-hover:scale-y-100"></span>
+        <span className="relative z-10 text-black group-hover:text-black transition-colors duration-300 ease-in-out">Sign In</span>
+      </button>
     </div>
   );
 };
